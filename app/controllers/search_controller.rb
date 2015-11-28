8ac0274885d78961a4217590a08e828a550e3b44
Asmatullah Noor <asmatullah.noor@haw-hamburg.de>
2015-11-28 16:10:47 +0100

    Fix ugly placeholders to placeholder conditions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
 
 	def find
-		@locations = Location.where("name LIKE ? OR city LIKE ?", "%#{params[:q]}%", "%#{params[:q]}%")
+		@locations = Location.where("name LIKE :query OR city LIKE :query", query: "%#{params[:q]}%")
 		@artists = Artist.where("name LIKE ?","%#{params[:q]}%")
-		@titles = Title.where("title LIKE ? OR album_name LIKE ? OR year LIKE ?", "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%")
+		@titles = Title.where("title LIKE :query OR album_name LIKE :query OR year LIKE :query", query: "%#{params[:q]}%")
 	end
 end
