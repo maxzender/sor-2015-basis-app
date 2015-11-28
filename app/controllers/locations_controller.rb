b80b79db74dcc186c2691816edfd7590fb972a53
Benjamin Singer MBA <Benni@Slim-Leo.local>
2015-11-28 13:27:23 +0100

    Location: - add New/Create Methods - add new View

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,4 +8,17 @@ class LocationsController < ApplicationController
     @location = Location.find(params[:id])
   end
 
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(params[:location])
+    if @location.save
+      redirect_to @location
+    else
+      render 'new'
+    end
+  end
+
 end
