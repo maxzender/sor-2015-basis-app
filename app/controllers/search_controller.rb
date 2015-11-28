2c62f8f483d0e4d41c5a257aa678ccc84dc53667
Asmatullah Noor <asmatullah.noor@haw-hamburg.de>
2015-11-28 16:00:26 +0100

    Fuzzy Search All completed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
 
 	def find
-		@results = Location.where("name LIKE ?", "%#{params[:q]}%")
+		@locations = Location.where("name LIKE ? OR city LIKE ?", "%#{params[:q]}%", "%#{params[:q]}%")
+		@artists = Artist.where("name LIKE ?","%#{params[:q]}%")
+		@titles = Title.where("title LIKE ? OR album_name LIKE ? OR year LIKE ?", "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%")
 	end
-
 end
