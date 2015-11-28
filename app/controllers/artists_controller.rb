b51a967872d46e56466f27acc47ece3abd557304
Benjamin Singer MBA <Benni@Slim-Leo.local>
2015-11-28 15:26:06 +0100

    Artists: - Controller Actions (new, create) - View (new)

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,4 +8,17 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(params[:artist])
+    if @artist.save
+      redirect_to @artist
+    else
+      render 'new'
+    end
+  end
+
 end
