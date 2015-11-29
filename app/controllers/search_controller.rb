741fb67628a47cbc8a67075dc64315563ea3fda6
Fahrradflucht <mail@mathiswiehl.de>
2015-11-29 10:39:38 +0100

    Fix for heroku search bug.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,6 @@ class SearchController < ApplicationController
 	def find
 		@locations = Location.where("name LIKE :query OR city LIKE :query", query: "%#{params[:q]}%")
 		@artists = Artist.where("name LIKE ?","%#{params[:q]}%")
-		@titles = Title.where("title LIKE :query OR album_name LIKE :query OR year LIKE :query", query: "%#{params[:q]}%")
+		@titles = Title.where("title LIKE :query OR album_name LIKE :query OR cast(year as text) LIKE :query", query: "%#{params[:q]}%")
 	end
 end
